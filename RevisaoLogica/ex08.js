c924de44c5f0efa69b8b0fa38c8b1d4ad05de1ba
wlfgrl417 <gabriel_sebrao@estudante.sesisenai.org.br>
2023-08-13 16:14:40 -0300

    commitado

--- RevisaoLogica/ex08.js
+++ RevisaoLogica/ex08.js
@@ -28,7 +28,7 @@
 
 function contarVogais(mensagem) {
     var quantidadeVogais = 0
-    var listaVogais = "aAeEiIoOuUáàâãéèêíìîóòõôùúû"
+    var listaVogais = "aAeEiIoOuUÁáÀàÂâÃãÉéÈèÊêÍíÌìîîÓóÒòÕõÔôÙùÚúÛû"
     for(var letra in mensagem)  {
         for(var vogal in listaVogais) {
             if(mensagem[letra] == listaVogais[vogal]) {
